3571184560181b9de80c6d57604d3390007ffc27
Muskan:) <108791578+Smilaayyyy@users.noreply.github.com>
2024-08-20 13:20:41 +0530

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
             <p className="home-description">
                 Join us on an adventurous journey with playful kittens! Navigate through the game and see how far you can go.
             </p>
-            <Link to="/gameboard">
+            <Link to="/Gameboard">
                 <button className="btn start-game-btn">Start the Adventure</button>
             </Link>
         </div>
